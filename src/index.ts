5471197bff0e2819f85ff4152c9ddf77815da095
Martin Veith <ndresx@gmail.com>
2019-06-17 22:12:16 +0200

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './reducer';
+import reducer, { defaultState } from './reducer';
 import * as actionTypes from './action_types';
 import * as actions from './actions';
 import * as selectors from './selectors';
@@ -6,4 +6,5 @@ import * as utils from './utils';
 import Op from './op';
 export * from './typedefs';
 
-export { actionTypes, actions, selectors, utils, Op };
+export { defaultState, actionTypes, actions, selectors, utils, Op };
+export default reducer;
